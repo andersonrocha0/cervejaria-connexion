7bade3ee8ea2faa455d88c6f7b1a1d6c09bdca20
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2020-09-29 14:52:26 -0300

    Adiciona features de CRUD para a API Rest de Cervejas

--- server/controllers/cervejas_controller.py
+++ server/controllers/cervejas_controller.py
@@ -1,9 +1,30 @@
 from server.services import cerveja_service
 
 
-def get_cervejas():
-    return cerveja_service.get_cervejas(), 200
+def get_cervejas(teor_alcoolico_min=None, teor_alcoolico_max=None):
+    return cerveja_service.get_cervejas(teor_alcoolico_min, teor_alcoolico_max), 200
 
 
 def post_cerveja(cerveja_input):
     return cerveja_service.post_cerveja(cerveja_input), 201
+
+
+def get_cerveja(id_cerveja):
+    cerveja = cerveja_service.get_cerveja(id_cerveja)
+    return handle_cerveja(cerveja)
+
+
+def handle_cerveja(cerveja):
+    if cerveja is not None:
+        return cerveja, 200
+    else:
+        return None, 404
+
+
+def delete_cerveja(id_cerveja):
+    cerveja_service.delete_cerveja(id_cerveja), 204
+
+
+def put_cerveja(id_cerveja, cerveja_input):
+    cerveja = cerveja_service.put_cerveja(id_cerveja, cerveja_input)
+    return handle_cerveja(cerveja)
--- server/services/cerveja_service.py
+++ server/services/cerveja_service.py
@@ -1,9 +1,19 @@
 from server import Cerveja, cervejas
-from server.repositories import cerveja_repository
 
 
-def get_cervejas():
-    return cerveja_repository.find_all_cervejas()
+def get_cerveja_entre_min_max(cerveja: Cerveja, teor_min, teor_max):
+    if teor_min <= cerveja.teor_alcoolico <= teor_max:
+        return True
+
+
+def get_cervejas(teor_alcoolico_min=None, teor_alcoolico_max=None):
+    cervejas_para_retornar = cervejas
+    if teor_alcoolico_min is not None and teor_alcoolico_max is not None:
+        cervejas_para_retornar = list(
+            filter(lambda cerveja: get_cerveja_entre_min_max(cerveja, teor_alcoolico_min, teor_alcoolico_max),
+                   cervejas))
+
+    return cervejas_para_retornar
 
 
 def post_cerveja(cerveja_input):
@@ -18,4 +28,34 @@ def post_cerveja(cerveja_input):
 
 
 def get_max_id_mais_um() -> int:
-    return max([c.id for c in cervejas]) + 1
+    return max([c.id for c in cervejas]) + 1 if len(cervejas) > 0 else 1
+
+
+def get_cerveja(id_cerveja):
+    cerveja = next(filter(lambda x: x.id == id_cerveja, cervejas), None)
+    return cerveja
+
+
+def delete_cerveja(id_cerveja):
+    i = 0
+    remover = False
+    for cerveja in cervejas:
+        if id_cerveja == cerveja.id:
+            remover = True
+            break
+        i = i + 1
+
+    if remover:
+        del cervejas[i]
+
+
+def put_cerveja(id_cerveja, cerveja_input):
+    cerveja = get_cerveja(id_cerveja)
+    if cerveja is None:
+        return None
+
+    cerveja.estilo = cerveja_input['estilo']
+    cerveja.nome = cerveja_input['nome']
+    cerveja.teor_alcoolico = cerveja_input['teor_alcoolico']
+
+    return cerveja
