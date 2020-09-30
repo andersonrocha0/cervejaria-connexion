73240dbc645db9d6f01c8b564218128a8cf5121c
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2020-09-30 15:42:07 -0300

    (feat) Aplicação configurada para trabalhar com banco de dados postgres.
    Criamos o primeiro modelo de dados utilizando técnicas de ORM e Migrations.

--- server/__main__.py
+++ server/__main__.py
@@ -1,9 +1,19 @@
 import connexion
 
+from server import db
+
 
 def start_server():
     app = connexion.App(__name__, specification_dir='swagger/')
     app.add_api('swagger.yaml')
+
+    app.app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:1234@localhost:5432/postgres'
+    app.app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.app.config["SQLALCHEMY_ECHO"] = True
+    app.app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {'pool_pre_ping': True}
+
+    db.init_app(app.app)
+
     app.run(port=8080)
 
 
--- server/models/cerveja.py
+++ server/models/cerveja.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+from sqlalchemy import Column, Integer, String, Numeric
+
+from server import db
+
 
 @dataclass
 class Cerveja:
@@ -7,3 +11,12 @@ class Cerveja:
     estilo: str = None
     nome: str = None
     teor_alcoolico: float = None
+
+
+class CervejaEntity(db.Model):
+    __tablename__ = 'tb_cerveja'
+
+    id = Column(Integer, primary_key=True)
+    estilo = Column(String)
+    nome = Column(String)
+    teor_alcoolico = Column(Numeric)
