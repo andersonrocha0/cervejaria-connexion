a3821991171494f0f78b8e948f98a087511fe52b
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2020-10-07 16:49:59 -0300

    (feat) Adicionado TOX para realizar testes e gerar relatório de cobertura.

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="cervejas",  # Replace with your own username
+    version="0.0.1",
+    author="Anderson Rocha",
+    author_email="anderson.rocha@addesistemas.com.br",
+    description="API de Cervejas",
+    long_description="API de Cervejas",
+    url="https://github.com/andersonrocha0/cervejas-connexion",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
